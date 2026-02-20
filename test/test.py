6b41ae2c494d7ec30ee5c829956f043729bb7918
anna-vee <starringmangoes@gmail.com>
2026-02-20 10:58:47 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -9,19 +9,15 @@ from cocotb.triggers import ClockCycles
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
-    # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
-    # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-
-    dut._log.info("Test project behavior")
+    dut._log.info("Test passed")
 
     # Set the input values you want to test
    
