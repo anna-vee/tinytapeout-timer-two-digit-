80c549f76c5f92659575bd9ed005665c61032f1d
anna-vee <starringmangoes@gmail.com>
2026-02-20 14:32:13 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -2,14 +2,11 @@
  * Copyright (c) 2024 ANNA V
  * SPDX-License-Identifier: Apache-2.0
  */
-
-
 /*
  * Copyright (c) 2024 Anna V
  * SPDX-License-Identifier: Apache-2.0
  */
 `default_nettype none
-
 module tt_um_anna_vee (
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
@@ -20,10 +17,8 @@ module tt_um_anna_vee (
     input  wire       clk,
     input  wire       rst_n
 );
-
   wire button = ui_in[1];
   wire switch = ui_in[2];
-
   reg a, b, c, d, e, f, g, dig1, dig2;
   assign uo_out  = {1'b0, g, f, e, d, c, b, a};
   assign uio_out = {6'b0, dig2, dig1};
@@ -31,14 +26,27 @@ module tt_um_anna_vee (
 
   reg [3:0] ones = 0;
   reg [3:0] tens = 0;
+  reg [9:0] debounce_cnt = 0;
+  reg button_stable = 0;
   reg button_prev = 0;
   reg [9:0] muxswitch = 0;
   reg mux = 0;
   reg [22:0] seconds = 0;
 
   always @(posedge clk) begin
-    button_prev <= button;
-    if (button && !button_prev) begin
+    button_prev <= button_stable;
+
+    if (button == 1'b1) begin
+      if (debounce_cnt == 10'd999)
+        button_stable <= 1;
+      else
+        debounce_cnt <= debounce_cnt + 1;
+    end else begin
+      debounce_cnt <= 0;
+      button_stable <= 0;
+    end
+
+    if (button_stable && !button_prev) begin
       if (ones == 9) begin
         ones <= 0;
         if (tens == 9)
@@ -49,9 +57,11 @@ module tt_um_anna_vee (
         ones <= ones + 1;
       end
     end
+
     muxswitch <= muxswitch + 1;
     if (muxswitch == 0)
       mux <= ~mux;
+
     if (switch) begin
       seconds <= seconds + 1;
       if (seconds == 6000000) begin
