133a589dd787373eebee5690193caeee5e2c74c6
Annika <starringmangoes@gmail.com>
2026-05-09 13:15:07 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,25 +1,38 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
+*/
+module tb ();
 
-@cocotb.test()
-async def test_reset(dut):
-    # 100 kHz = 10 µs period
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
+  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
+  initial begin
+    $dumpfile("tb.fst");
+    $dumpvars(0, tb);
+    #1;
+  end
 
-    # Assert reset
-    dut.rst_n.value = 0
-    dut.ui_in.value = 0
-    await ClockCycles(dut.clk, 5)
+  // Wire up the inputs and outputs:
+  reg clk;
+  reg rst_n;
+  reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
 
-    # Release reset
-    dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 10)
+  // Replace tt_um_example with your module name:
+  tt_um_example user_project (
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (ena),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
+  );
 
-    # After reset, outputs should be 0
-    assert dut.uo_out.value == 0
-    assert dut.uio_out.value == 0
+endmodule
