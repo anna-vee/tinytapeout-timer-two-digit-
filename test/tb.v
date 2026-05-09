23e60502b35738e88e7b245dc24afba46dcb3654
Annika <starringmangoes@gmail.com>
2026-05-09 13:09:04 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,9 +1,6 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
